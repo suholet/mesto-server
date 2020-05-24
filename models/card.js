f8bc39f05462fcabccc5d15166492a1b6b6cd9ce
Dmitry Suholet <suholet@gmail.com>
2020-05-24 17:55:05 +0300

    Add card schema

---
+++ models/card.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^(ftp|http|https):\/\/[^ "]+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid link!`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  likes: {
+    type: mongoose.Schema.Types.ObjectId,
+    enum: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
