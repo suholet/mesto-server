80c08193d7359507031f2a1a988b4ac60bdda963
Dmitry Suholet <suholet@gmail.com>
2020-05-27 10:53:44 +0300

    Add helmet and express-rate-limit

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const router = require('./routes/router');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
