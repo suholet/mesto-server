e2eb79e2ff63bb67af6f80ac6a8ebd4dc6af03c4
Dmitry Suholet <suholet@gmail.com>
2020-06-01 00:29:59 +0300

    Add login method to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports.getUsers = (req, res, next) => {
@@ -69,4 +70,25 @@ module.exports.updateAvatar = (req, res, next) => {
       console.log('Что-то пошло не так при обновлении аватара пользователя. ', err);
       next(err);
     });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      // Аутентификация успешна
+      const token = jwt.sign(
+        { _id: user._id },
+        'HASH',
+        { expiresIn: '7d' },
+      );
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      })
+        .end();
+      // res.send({ token });
+    })
+    .catch((err) => res.status(401).send({ message: err.message }));
 };
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { isEmail, isURL } = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -28,6 +29,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     validate: {
       validator(v) {
         return isEmail(v);
@@ -38,7 +40,25 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    minlength: 8,
   },
 });
+
+// eslint-disable-next-line func-names
+userSchema.static.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильная почта или пароль!'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильная почта или пароль!'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
