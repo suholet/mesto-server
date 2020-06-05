913432701c6a008494f5245c215f526817bff961
Dmitry Suholet <suholet@gmail.com>
2020-06-05 21:52:19 +0300

    Add CSRF protection

--- controllers/users.js
+++ controllers/users.js
@@ -85,6 +85,7 @@ module.exports.login = (req, res) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
+        sameSite: true,
       })
         .end();
       // res.send({ token });
